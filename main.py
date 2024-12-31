94f1e7aa6ca7c1ef9dcaa02d70c490fcfeb01cbf
Poojan Khamar <poojan-khamar@Poojans-Mac-mini.local>
2024-12-31 13:03:47 +0530

    stt

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import torch
 from transformers import pipeline
 
 model = "meta-llama/Meta-Llama-3-8B"
-generator = pipeline("text-generation", model=model).to("cuda")
+generator = pipeline("text-generation", model=model, device="mps")
 input_text = "Once upon a time"
-generated_text = generator(input_text, max_length=100, num_return_sequences=1)
+generated_text = generator(input_text,  num_return_sequences=1)
 
 print(generated_text)
